eef0b993fcc297d03fc3806ceafdcd45d7ab9502
Praditya <adit@butternutwebsite.com>
2019-03-30 15:08:42 +0700

    add gtm plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,5 +54,19 @@ module.exports = {
         format: 'ddd, DD MMM YYYY hh:mm A',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-KDHCVL7",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // Specify optional GTM environment details.
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+      },
+    },
   ],
 }
