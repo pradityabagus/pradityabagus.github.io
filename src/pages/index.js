57e36469d87e8937307c8a5acf0a57a4cca2fdc0
Praditya B. Dewantara <dewantara@live.com>
2020-11-18 22:26:39 +0700

    video link correction

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     
     <p>Hi there, I'm Praditya Bagus Dewantara. You can call me Adit, Dewo, or anyway you like it. This page is the only thing I can assure you I maintain and own.</p>
 
-    <iframe width="560" height="315" src="https://www.youtube.com/watch?v=QMP2BsGoJNg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>  
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/QMP2BsGoJNg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>  
     
     <p><Link to="/contact-me/">Ping me!</Link></p>
   </Layout>
