5393aeb50f4af024f438dbdd6ab6b8738124a22d
Praditya B. Dewantara <dewantara@live.com>
2020-12-16 17:21:58 +0700

    contact updates
    in an effort to keeping myself alive

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -6,11 +6,9 @@ import { withPrefix } from 'gatsby'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Ping me!</h1>
-    <p>You can reach me via email to dewantara@live.com</p>
-    <p>
-      I'm currently off the social network grid.
-    </p>
+    <h1>I am sorry...</h1>
+    <p>I am currently experiencing mental instability. In order to keep my sanity, I am completely off all personal communication channel. Including social media and email. I'll update my new contact info in this page. Thank you.</p>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
