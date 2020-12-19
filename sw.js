5135cebec9d739e59ead2d0604ad3749081e1239
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-19 14:48:02 +0000

    Deploy pradityabagus/pradityabagus.github.io to github.com/pradityabagus/pradityabagus.github.io.git:master

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0590b5b3abf6957afae5.js"
+    "url": "webpack-runtime-8bd4b12e67db8c5e8a75.js"
   },
   {
     "url": "app-fb38911ba0a349c85063.js"
@@ -36,24 +36,24 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "d3720e77cb4eb96e0d0f03e6c95fabdd"
+    "revision": "cf9707d5b4c34acf7175b2ba9be323f0"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "18947faf20920ffb10420d5fd9f65ddb"
+    "revision": "13b1960de2126e0c43ee97132ad9bf9d"
   },
   {
     "url": "0-b662b2c38aac3f2c6516.js"
   },
   {
-    "url": "component---src-pages-index-js-456781014fd0c5f986b8.js"
+    "url": "component---src-pages-index-js-063be756c3b5d6a0ebf6.js"
   },
   {
     "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
     "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
   },
   {
-    "url": "component---src-pages-404-js-7529993df783c9447dd6.js"
+    "url": "component---src-pages-404-js-56c046f4534c5fe35747.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
